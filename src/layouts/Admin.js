c3c91bd6fa82e02ed20a95f1cb9cb7fe6a5fd496
kavxya <e0550269@u.nus.edu>
2022-10-28 15:42:59 +0800

    Populates job data, job listing page with jobs. Implements filtering of tags.

--- src/layouts/Admin.js
+++ src/layouts/Admin.js
@@ -5,7 +5,7 @@ import { Switch, Route, Redirect } from "react-router-dom";
 
 import AdminNavbar from "components/Navbars/AdminNavbar.js";
 import Sidebar from "components/Sidebar/Sidebar.js";
-import HeaderStats from "components/Headers/HeaderStats.js";
+//import HeaderStats from "components/Headers/HeaderStats.js";
 import FooterAdmin from "components/Footers/FooterAdmin.js";
 
 // views
@@ -21,8 +21,11 @@ export default function Admin() {
       <Sidebar />
       <div className="relative md:ml-64 bg-blueGray-100">
         <AdminNavbar />
-        {/* Header */}
-        <HeaderStats />
+        {/* Header Ribbon <HeaderStats />*/}
+
+        <div className="relative bg-lightBlue-600 md:pt-32 pb-20 pt-12 ">
+        </div>
+    
         <div className="px-4 md:px-10 mx-auto w-full -m-24">
           <Switch>
             <Route path="/admin/dashboard" exact component={Dashboard} />
--- src/views/admin/Dashboard.js
+++ src/views/admin/Dashboard.js
@@ -1,30 +1,92 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import data from '../../data/jobsData.js'
 
 // components
 
-import CardLineChart from "components/Cards/CardLineChart.js";
-import CardBarChart from "components/Cards/CardBarChart.js";
-import CardPageVisits from "components/Cards/CardPageVisits.js";
-import CardSocialTraffic from "components/Cards/CardSocialTraffic.js";
+//import CardLineChart from "components/Cards/CardLineChart.js";
+//import CardBarChart from "components/Cards/CardBarChart.js";
+//import CardPageVisits from "components/Cards/CardPageVisits.js";
+//import CardSocialTraffic from "components/Cards/CardSocialTraffic.js";
+import JobBoardCard from "components/JobBoardCard/JobBoardCard";
 
 export default function Dashboard() {
+  const [jobs,setJobs] = useState([]);
+  const [filters, setFilters] = useState([]);
+
+  /*Change to api call for the data*/
+  useEffect(() => setJobs(data), []);
+
+  const filterFunc = ({role, level, tools, languages,skillset}
+  ) => {
+    if(filters.length ===0) {
+      return true;
+    }
+
+    const tags = [role,level];
+
+    if (tools){
+      tags.push(...tools);
+  }
+
+  if (languages){
+      tags.push(...languages);
+  }
+
+  if (skillset){
+      tags.push(...skillset);
+  }
+
+    return tags.some(tag => filters.includes(tag));
+  }
+
+  const handleTagClick = (tag) => {
+    setFilters([...filters,tag]);
+  }
+
+  const handleFilterClick = (passedFilter) => {
+    setFilters(filters.filter(f => f!== passedFilter))
+  }
+
+  const filteredJobs = jobs.filter(filterFunc);
+
+  console.log(jobs);
+
   return (
     <>
-      <div className="flex flex-wrap">
-        <div className="w-full xl:w-8/12 mb-12 xl:mb-0 px-4">
-          <CardLineChart />
+      <div className = "w-full xl:w-8/12 mb-12 xl:mb-0 px-4">
+        <h2 className="relative text-white text-xl font-semibold"> 
+          Job Listings 
+        </h2>
+        <h1 className="h-20"> Edit this to make a padding. </h1>
+
+        <div className ='flex bg-white shadow-md m-4 p-5 rounded align-middle'>
+          {
+            filters.length === 0 ?(
+              <p className="font-light"> Click on a Tag to Apply filter </p>
+            ):filters.map(
+              (filter) => 
+                <span onClick={() =>
+                  handleFilterClick(filter)}
+                  className="text-lightBlue-600 bg-lightBlue-100 m-2 p-2 rounded-full text-center text-xs font-semibold"
+                  >
+                  {filter}
+                  <span className='text-lightBlue-600 -mr-4 font-semibold text-lg-special pl-2h pr-1'>x</span>
+                </span>)
+          }
+          
         </div>
-        <div className="w-full xl:w-4/12 px-4">
-          <CardBarChart />
-        </div>
-      </div>
-      <div className="flex flex-wrap mt-4">
-        <div className="w-full xl:w-8/12 mb-12 xl:mb-0 px-4">
-          <CardPageVisits />
-        </div>
-        <div className="w-full xl:w-4/12 px-4">
-          <CardSocialTraffic />
-        </div>
+        {
+          jobs.length === 0 ? (
+            <p> Jobs are loading... </p>
+          ):(
+            filteredJobs.map((job)=> 
+            <JobBoardCard 
+              job ={job} 
+              key = {job.id} 
+              handleTagClick ={handleTagClick}
+            />
+          ))
+        }
       </div>
     </>
   );
