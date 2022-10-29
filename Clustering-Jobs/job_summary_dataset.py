23e650a0ec30635653a60de9683eaf697327a839
BharathSShankar <coolbharath2@gmail.com>
2022-10-29 14:36:50 +0800

    More additions to transfer learning scripts

--- Clustering-Jobs/job_summary_dataset.py
+++ Clustering-Jobs/job_summary_dataset.py
@@ -1,15 +1,22 @@
 from torch.utils.data import Dataset
 from torch import device
+import torch
 import pandas as pd
 
-class JobSummaryLoader(Dataset):
+class JobSummaryDataset(Dataset):
     
-    def __init__(self, data_src:str, device:device):
-        self.data = pd.read_csv(data_src)
-        self.device = device
+    def __init__(self, data_src:str, tokenizer):
+        data = pd.read_excel(data_src)
+        texts = data.iloc[:, 7]
+        summ = data.iloc[:, 8]
+        self.encodings = tokenizer(texts, truncation=True, padding=True , return_tensors="pt").input_ids
+        self.labels = tokenizer(summ, truncation = True, padding = True, return_tensors="pt").input_ids
     
     def __getitem__(self, idx:int):
-        return (self.data.iloc[idx, 7], self.data.iloc[idx, 8])
+        item = {}
+        item["input_ids"] = self.encodings[idx]
+        item['labels'] = self.labels[idx]
+        return item  
     
     def __len__(self):
         return len(self.data)
--- Clustering-Jobs/jobs_preprocessing.py
+++ Clustering-Jobs/jobs_preprocessing.py
@@ -1,6 +1,43 @@
-import pandas as pd
-import transformers
+from job_summary_dataset import JobSummaryDataset
+import torch
+from transformers import AutoTokenizer, AutoModel
+from transformers import AutoModelForSeq2SeqLM
+from transformers import DataCollatorForSeq2Seq
+from transformers import Seq2SeqTrainer
 
-dat = pd.read_excel("Clustering-Jobs/data/tc_job_data.xls")
+dat = "Clustering-Jobs/data/tc_jobs_data.xls"
 
+max_input_length = 768
+max_target_length = 256
 
+tokenizer = AutoTokenizer.from_pretrained("t5-small", max_length = max_input_length)
+
+dataset = JobSummaryDataset(dat, tokenizer)
+
+model = AutoModelForSeq2SeqLM.from_pretrained('t5-small')
+
+from transformers import Seq2SeqTrainingArguments
+
+batch_size = 8
+num_train_epochs = 1
+model_name = 'mt5-small-model'
+
+args = Seq2SeqTrainingArguments(
+    output_dir=f"Clustering-Jobs/DL-Models/{model_name}-V1",
+    evaluation_strategy="epoch",
+    learning_rate=5.6e-5,
+    per_device_train_batch_size=batch_size,
+    per_device_eval_batch_size=batch_size,
+    weight_decay=0.01,
+    save_total_limit=3,
+    num_train_epochs=num_train_epochs,
+    predict_with_generate=True,
+)
+
+trainer = Seq2SeqTrainer(
+    model,
+    args,
+    train_dataset=dataset,
+)
+
+trainer.train()
