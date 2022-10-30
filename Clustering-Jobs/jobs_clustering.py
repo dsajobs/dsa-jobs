454583dac3974c30b318f7eb3c3ba43f6e8821f8
BharathSShankar <coolbharath2@gmail.com>
2022-10-30 19:19:08 +0800

    Clustered 1537 jobs

--- Clustering-Jobs/jobs_clustering.py
+++ Clustering-Jobs/jobs_clustering.py
@@ -1,14 +1,33 @@
-from os import truncate
+from sklearn.cluster import KMeans
+import pandas as pd
 from transformers import AutoModel, AutoTokenizer
 import torch
+import numpy as np
+from tqdm import tqdm
 
-def encode_list(model_path:str, job_desc):
-    tokenizer = AutoTokenizer.from_pretrained('t5-small',
+model = AutoModel.from_pretrained("Clustering-Jobs/DL-Models")
+tokenizer = AutoTokenizer.from_pretrained('t5-small',
      truncate = True,
       padding = True)
+def encode_list(model:str, job_desc): 
     job_desc = tokenizer.encode(job_desc, return_tensors = 'pt')
-    model = AutoModel.from_pretrained(model_path)
     return torch.mean(model.encoder(input_ids = 
     job_desc).last_hidden_state, dim = 1)[0].detach().numpy()
 
-print(encode_list("./Clustering-Jobs/DL-Models/", "Hello Guys"))
+linkedin_jobs = pd.read_csv("Clustering-Jobs/data/linkedin_jobs.csv", encoding='latin-1')["Job Description"].dropna()
+jdb_bi = pd.read_csv("Clustering-Jobs/data/output-jdb-bi.csv")["Job Description"].dropna()
+jdb_da = pd.read_csv("Clustering-Jobs/data/output-jdb-da.csv",  delimiter=';')["Job Description"].dropna()
+jdb_ds = pd.read_csv("Clustering-Jobs/data/output-jdb-ds.csv")["Job Description"].dropna()
+
+out_dict = {"job_desc" : [], "encoding" : []}
+
+for i in [linkedin_jobs, jdb_bi, jdb_da, jdb_ds]:
+    out_dict["job_desc"].extend(list(i))
+    for s in tqdm(list(i)):
+      out_dict["encoding"].append(encode_list(model, s))
+
+out = pd.DataFrame.from_dict(out_dict)
+
+out["labels"] = KMeans(n_clusters = 3, random_state=3101).fit(np.array(out_dict["encoding"])).labels_
+
+out.to_csv("Clustering-Jobs/data/processed_jobs.csv")
