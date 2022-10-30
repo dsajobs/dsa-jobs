88aa5d8049abe805a8306b3c905e2677a6213403
BharathSShankar <coolbharath2@gmail.com>
2022-10-30 14:59:24 +0800

    Added jobs clustering

---
+++ Clustering-Jobs/jobs_clustering.py
@@ -0,0 +1,13 @@
+from os import truncate
+from transformers import AutoModel, AutoTokenizer
+import torch
+
+def encode_list(model_path:str, job_desc):
+    tokenizer = AutoTokenizer.from_pretrained('t5-small',
+     truncate = True,
+      padding = True)
+    job_desc = tokenizer.encode(job_desc, return_tensors = 'pt')
+    model = AutoModel.from_pretrained(model_path)
+    return torch.mean(model.encoder(input_ids = job_desc).last_hidden_state, dim = 1)[0]
+
+print(encode_list("./Clustering-Jobs/DL-Models/", "Hello Guys"))
