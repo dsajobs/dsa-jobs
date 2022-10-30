01a49496d6c93194e04924fb6e15f4dc15bfcd7b
BharathSShankar <coolbharath2@gmail.com>
2022-10-30 16:23:38 +0800

    Forgot .detach

--- Clustering-Jobs/jobs_clustering.py
+++ Clustering-Jobs/jobs_clustering.py
@@ -8,6 +8,7 @@ def encode_list(model_path:str, job_desc):
       padding = True)
     job_desc = tokenizer.encode(job_desc, return_tensors = 'pt')
     model = AutoModel.from_pretrained(model_path)
-    return torch.mean(model.encoder(input_ids = job_desc).last_hidden_state, dim = 1)[0].numpy()
+    return torch.mean(model.encoder(input_ids = 
+    job_desc).last_hidden_state, dim = 1)[0].detach().numpy()
 
 print(encode_list("./Clustering-Jobs/DL-Models/", "Hello Guys"))
