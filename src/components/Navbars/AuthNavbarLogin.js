2107fb40097d386df18a91e50b43d50206690a40
DESKTOP-4CTVJ2I\nicho <nicholas.tanws@yahoo.com.sg>
2022-11-07 00:10:02 +0800

    Added AuthNavbarLogin and Navbar. Updated AuthNavbar

--- src/components/Navbars/AuthNavbarLogin.js
+++ src/components/Navbars/AuthNavbarLogin.js
@@ -2,6 +2,7 @@
 import React from "react";
 import { Link } from "react-router-dom";
 
+
 // components
 
 import PagesDropdown from "components/Dropdowns/PagesDropdown.js";
@@ -13,18 +14,22 @@ export default function Navbar(props) {
       <nav className="top-0 absolute z-50 w-full flex flex-wrap items-center justify-between px-2 py-3 navbar-expand-lg">
         <div className="container px-4 mx-auto flex flex-wrap items-center justify-between">
           <div className="w-full relative flex justify-between lg:w-auto lg:static lg:block lg:justify-start">
+            <img 
+            alt="..."
+            className="w-5,mr-1"
+            src={require("../../assets/img/NUSLogo2.png").default}
+            />
             <Link
-              className="text-white text-sm font-bold leading-relaxed inline-block mr-4 py-2 whitespace-nowrap uppercase"
+              className="text-black text-sm font-bold leading-relaxed inline-block mr-4 py-2 whitespace-nowrap uppercase"
               to="/"
             >
-              Notus React
             </Link>
             <button
               className="cursor-pointer text-xl leading-none px-3 py-1 border border-solid border-transparent rounded bg-transparent block lg:hidden outline-none focus:outline-none"
               type="button"
               onClick={() => setNavbarOpen(!navbarOpen)}
             >
-              <i className="text-white fas fa-bars"></i>
+              <i className="text-black fas fa-bars"></i>
             </button>
           </div>
           <div
@@ -40,7 +45,7 @@ export default function Navbar(props) {
                   className="lg:text-white lg:hover:text-blueGray-200 text-blueGray-700 px-3 py-4 lg:py-2 flex items-center text-xs uppercase font-bold"
                   href="https://www.creative-tim.com/learning-lab/tailwind/react/overview/notus?ref=nr-auth-navbar"
                 >
-                  <i className="lg:text-blueGray-200 text-blueGray-400 far fa-file-alt text-lg leading-lg mr-2" />{" "}
+                  <i className="lg:text-black-200 text-blueGray-400 far fa-file-alt text-lg leading-lg mr-2" />{" "}
                   Docs
                 </a>
               </li>
@@ -48,16 +53,15 @@ export default function Navbar(props) {
             <ul className="flex flex-col lg:flex-row list-none lg:ml-auto">
               {/* <li className="flex items-center">
                 <PagesDropdown />
-              </li>  */}
+              </li> */}
               {/* <li className="flex items-center">
                 <a
                   className="lg:text-white lg:hover:text-blueGray-200 text-blueGray-700 px-3 py-4 lg:py-2 flex items-center text-xs uppercase font-bold"
-                //   href="https://www.facebook.com/sharer/sharer.php?u=https%3A%2F%2Fdemos.creative-tim.com%2Fnotus-react%2F%23%2F"
+                  href="https://www.facebook.com/sharer/sharer.php?u=https%3A%2F%2Fdemos.creative-tim.com%2Fnotus-react%2F%23%2F"
                   target="_blank"
-                  to="/admin/settings"
                 >
-                  {/* <i className="lg:text-blueGray-200 text-blueGray-400 fab fa-facebook text-lg leading-lg " /> 
-                  <span className="lg:hidden inline-block ml-2">Settings</span>
+                  <i className="lg:text-blueGray-200 text-blueGray-400 fab fa-facebook text-lg leading-lg " />
+                  <span className="lg:hidden inline-block ml-2">Share</span>
                 </a>
               </li>
 
@@ -81,35 +85,7 @@ export default function Navbar(props) {
                   <i className="lg:text-blueGray-200 text-blueGray-400 fab fa-github text-lg leading-lg " />
                   <span className="lg:hidden inline-block ml-2">Star</span>
                 </a>
-              </li>  */}
-
-
-              <Link
-                to="/admin/dashboard"
-                className={
-                  "lg:text-white lg:hover:text-blueGray-200 text-blueGray-700 px-3 py-4 lg:py-2 flex items-center text-xs uppercase font-bold"
-                }
-              >
-                Job Listings
-              </Link>
-
-              <Link
-                to="/admin/settings"
-                className={
-                  "lg:text-white lg:hover:text-blueGray-200 text-blueGray-700 px-3 py-4 lg:py-2 flex items-center text-xs uppercase font-bold"
-                }
-              >
-                Settings
-              </Link>
-
-              <Link
-                to="/profile"
-                className={
-                  "lg:text-white lg:hover:text-blueGray-200 text-blueGray-700 px-3 py-4 lg:py-2 flex items-center text-xs uppercase font-bold"
-                }
-              >
-                Profile
-              </Link>
+              </li> */}
 
               <li className="flex items-center">
                 <button
@@ -117,9 +93,9 @@ export default function Navbar(props) {
                   type="button"
                 >
                 <Link
-                  to="/landing"
+                  to="/auth/login"
                 >
-                  Logout
+                  Login
                 </Link>
                   {/* <i className="fas fa-arrow-alt-circle-down"></i> Login */}
                 </button>
--- src/components/Navbars/Navbar.js
+++ src/components/Navbars/Navbar.js
@@ -2,6 +2,7 @@
 import React from "react";
 import { Link } from "react-router-dom";
 
+
 // components
 
 import PagesDropdown from "components/Dropdowns/PagesDropdown.js";
@@ -12,19 +13,23 @@ export default function Navbar(props) {
     <>
       <nav className="top-0 absolute z-50 w-full flex flex-wrap items-center justify-between px-2 py-3 navbar-expand-lg">
         <div className="container px-4 mx-auto flex flex-wrap items-center justify-between">
-          <div className="w-full relative flex justify-between lg:w-auto lg:static lg:block lg:justify-start">
+          <div className="w-full relative flex justify-auto lg:w-auto lg:static lg:block lg:justify-start">
+            <img 
+            alt="..."
+            className="w-5,mr-1"
+            src={require("../../assets/img/NUSLogo2.png").default}
+            />
             <Link
-              className="text-white text-sm font-bold leading-relaxed inline-block mr-4 py-2 whitespace-nowrap uppercase"
+              className="text-black text-sm font-bold leading-relaxed inline-block mr-4 py-2 whitespace-nowrap uppercase"
               to="/"
             >
-              Notus React
             </Link>
             <button
               className="cursor-pointer text-xl leading-none px-3 py-1 border border-solid border-transparent rounded bg-transparent block lg:hidden outline-none focus:outline-none"
               type="button"
               onClick={() => setNavbarOpen(!navbarOpen)}
             >
-              <i className="text-white fas fa-bars"></i>
+              <i className="text-black fas fa-bars"></i>
             </button>
           </div>
           <div
@@ -40,7 +45,7 @@ export default function Navbar(props) {
                   className="lg:text-white lg:hover:text-blueGray-200 text-blueGray-700 px-3 py-4 lg:py-2 flex items-center text-xs uppercase font-bold"
                   href="https://www.creative-tim.com/learning-lab/tailwind/react/overview/notus?ref=nr-auth-navbar"
                 >
-                  <i className="lg:text-blueGray-200 text-blueGray-400 far fa-file-alt text-lg leading-lg mr-2" />{" "}
+                  <i className="lg:text-black-200 text-blueGray-400 far fa-file-alt text-lg leading-lg mr-2" />{" "}
                   Docs
                 </a>
               </li>
