e1fd0a35369496b4b48d835f5eab9e9a58f93471
Megan Michael <102518396+meganmeow@users.noreply.github.com>
2022-11-07 00:09:13 +0800

    changes resume upload function

--- src/views/Profile.js
+++ src/views/Profile.js
@@ -1,9 +1,43 @@
-import React from "react";
+import { Link } from "react-router-dom";
+import React, { useRef } from "react";
+import axios from 'axios';
+import useFileUpload from 'react-use-file-upload';
 
 import Navbar from "components/Navbars/AuthNavbar.js";
 import Footer from "components/Footers/Footer.js";
 
 export default function Profile() {
+  const {
+    files,
+    fileNames,
+    fileTypes,
+    totalSize,
+    totalSizeInBytes,
+    handleDragDropEvent,
+    clearAllFiles,
+    createFormData,
+    setFiles,
+    removeFile,
+  } = useFileUpload();
+  
+  const inputRef = useRef();
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+  
+    const formData = createFormData();
+  
+    try {
+      // axios.post("../data/", formData, {
+      //   'content-type': 'multipart/form-data',
+      // });
+      localStorage.setItem("resume", formData);
+      
+      console.log(localStorage.getItem("resume"));
+    } catch (error) {
+      console.error('Failed to submit files.');
+    }
+  };
+
   return (
     <>
       <Navbar transparent />
@@ -55,13 +89,74 @@ export default function Profile() {
                       />
                     </div>
                   </div>
+                  
                   <div className="w-full lg:w-4/12 px-4 lg:order-3 lg:text-right lg:self-center">
                     <div className="py-6 px-3 mt-32 sm:mt-0">
                       <button
-                        className="bg-lightBlue-500 active:bg-lightBlue-600 uppercase text-white font-bold hover:shadow-md shadow text-xs px-4 py-2 rounded outline-none focus:outline-none sm:mr-2 mb-1 ease-linear transition-all duration-150"
+                        className="bg-blueGrey-500 active:bg-blueGrey-600 uppercase text-black font-bold hover:shadow-md shadow text-xs px-4 py-2 rounded outline-none focus:outline-none sm:mr-2 mb-1 ease-linear transition-all duration-150"
                         type="button"
                       >
                         Upload Resume
+                        <div className="form-container">
+                          {/* Display the files to be uploaded */}
+                          <div>
+                            <ul>
+                              {fileNames.map((name) => (
+                              <li key={name}>
+                              <span>{name}</span>
+
+                              <span onClick={() => removeFile(name)}>
+                                <i className="fa fa-times" />
+                              </span>
+                              </li>
+                              ))}
+                            </ul>
+
+                          {files.length > 0 && (
+                            <ul>
+                              <li>File types found: {fileTypes.join(', ')}</li>
+                              <li>Total Size: {totalSize}</li>
+                              <li>Total Bytes: {totalSizeInBytes}</li>
+
+                              <li className="clear-all">
+                                <button onClick={() => clearAllFiles()}>Clear All</button>
+                              </li>
+                            </ul>
+                            )}
+                        </div>
+
+                      {/* Provide a drop zone and an alternative button inside it to upload files. */}
+                      <div
+                        // css={Dropzone}
+                        onDragEnter={handleDragDropEvent}
+                        onDragOver={handleDragDropEvent}
+                        onDrop={(e) => {
+                          handleDragDropEvent(e);
+                          setFiles(e, 'a');
+                        }}
+                      >
+                        {/* <p>Drag and drop files here</p> */}
+
+                        <button class="bg-lightBlue-500 active:bg-lightBlue 600 text-white hover:shadow-md shadow tet-xs px-2 py-1 rounded outline-none 
+                        focus:outline-none sm:mr-2 mb-1 ease-linear transition-all duration-150" onClick={() => inputRef.current.click()}>Select files to upload</button>
+
+                        {/* Hide the crappy looking default HTML input */}
+                        <input
+                          ref={inputRef}
+                          type="file"
+                          multiple
+                          style={{ display: 'none' }}
+                          onChange={(e) => {
+                            setFiles(e, 'a');
+                            inputRef.current.value = null;
+                          }}
+                        />
+                      </div>
+                    </div>
+                    <div className="submit">
+                      <button class="bg-lightBlue-500 active:bg-lightBlue 600 uppercase text-white font-bold hover:shadow-md shadow tet-xs px-3 py-1
+                      rounded outline-none focus:outline-none sm:mr-2 mb-1 ease-linear transition-all duration-150" onClick={handleSubmit}>Submit</button>
+                    </div>
                       </button>
                     </div>
                   </div>
