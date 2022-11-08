9f83f5ff24e8e4e3e045b205a898b439820c7533
BharathSShankar <coolbharath2@gmail.com>
2022-11-08 15:35:40 +0800

    Fixed Docker Container

--- Clustering-People/Fine-Tuning-resume.py
+++ Clustering-People/Fine-Tuning-resume.py
@@ -2,5 +2,8 @@ import transformers
 import pandas as pd
 
 resume_data = pd.read_csv("Clustering-People/data/UpdatedResumeDataSet.csv")
-print(pd.get_dummies(data = resume_data, columns=["Category"]))
+resume_data_proc = pd.get_dummies(data = resume_data, columns=["Category"])
+
+
+
 
