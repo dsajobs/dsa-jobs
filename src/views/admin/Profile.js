83b23a23342e247b8db696b2cfb0d6d4ef14c064
kavxya <e0550269@u.nus.edu>
2022-11-10 03:17:16 +0800

    update major

--- src/views/admin/Profile.js
+++ src/views/admin/Profile.js
@@ -93,7 +93,7 @@ export default function Profile() {
                   </div>
                   <div className="mb-2 text-blueGray-600 mt-10">
                     <i className="fas fa-briefcase mr-2 text-lg text-blueGray-400"></i>
-                    Local Pool Player
+                    Data Science and Analytics
                   </div>
                   <div className="mb-2 text-blueGray-600">
                     <i className="fas fa-university mr-2 text-lg text-blueGray-400"></i>
